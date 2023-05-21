4b6305fcb11c4117b2b8a9fc2e41e15e797f9f64
Rob Brennan <rob@therobbrennan.com>
2023-05-21 15:30:01 -0700

    Add placeholder for data visualization

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
 'use client'
 import Header from '../components/header'
 
+import HockeyRinkOverlay from '../components/HockeyRinkOverlay/HockeyRinkOverlay'
+
 const PAGE_TITLE = "Welcome to Next.js v13"
 
-export default function TestMigrationPage() {
+export default function DefaultPage() {
+  const shotData = [
+    { type: 'shot', x: 20, y: 20 },
+    { type: 'shot', x: -12, y: -23 },
+    { type: 'shot', x: 2, y: 42 },
+    { type: 'shot', x: -87, y: -15 },
+  ]
+
   return (
     <>
       <Header />
@@ -11,6 +20,7 @@ export default function TestMigrationPage() {
       <p>
         Stay tuned for data visualization examples.
       </p>
+      <HockeyRinkOverlay shotData={shotData} />
     </>
   )
 }
