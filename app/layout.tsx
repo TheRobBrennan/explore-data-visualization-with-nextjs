efa1551f8bad0b6b0eb87e43054192499d24ca6d
Rob Brennan <rob@therobbrennan.com>
2023-05-21 20:55:25 -0700

    Simplify the app layout to not use providers for the client

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,11 @@
 // The root layout replaces the pages/_app.tsx and pages/_document.tsx files
 import "../styles/styles.css"
-import { Metadata } from 'next';
-import { Session } from 'next-auth'
-import { headers } from 'next/headers'
-import AuthContext from '../contexts/authentication/AuthContext'
-
-export const metadata: Metadata = {
-  title: '[DEMO] Explore Data Visualization with Next.js',
-  description: 'Explore Data Visualization with Next.js',
-}
-
-async function getSession(cookie: string): Promise<Session> {
-  const response = await fetch(`${process.env.NEXTAUTH_URL}/api/auth/session`, {
-    headers: {
-      cookie,
-    },
-  });
-
-  const session = await response.json();
-
-  return Object.keys(session).length > 0 ? session : null;
-}
 
 export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode,
 }) {
-  const session = await getSession(headers().get('cookie') ?? '');
   return (
     <html lang="en">
       {/*
@@ -36,9 +14,7 @@ export default async function RootLayout({
       */}
       <head />
       <body>
-        <AuthContext session={session}>
-          {children}
-        </AuthContext>
+        {children}
       </body>
     </html>
   )
