73a167d7c9a4db51402360d500b85fb426e30a64
Rob Brennan <rob@therobbrennan.com>
2023-05-21 22:17:42 -0700

    Don't export authOptions as a const unless you want to feel the wrath of next build

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,7 +3,7 @@ import GithubProvider from "next-auth/providers/github"
 
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
-export const authOptions: NextAuthOptions = {
+const authOptions: NextAuthOptions = {
   // https://next-auth.js.org/configuration/providers/oauth
   providers: [
     GithubProvider({
