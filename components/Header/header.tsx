24d2ac69c8aae844915d60feb2e512e8986b29b2
Rob Brennan <rob@therobbrennan.com>
2023-06-24 15:19:02 -0700

    Add a navigation item to link to our NHL API Performance page

--- components/Header/header.tsx
+++ components/Header/header.tsx
@@ -37,6 +37,9 @@ export default async function Header() {
             <Link href="/">Home</Link>
           </li>
           <li className={styles.navItem}>
+            <Link href="/nhl-api-performance">NHL API Performance</Link>
+          </li>
+          <li className={styles.navItem}>
             {!session && (
               <SignInButton />
             )}
