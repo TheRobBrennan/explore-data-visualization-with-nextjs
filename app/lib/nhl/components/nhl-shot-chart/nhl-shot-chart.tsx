ccfe37b3e71dc348b313ce494da8adf7fcf6a70b
Rob Brennan <rob@therobbrennan.com>
2023-06-24 20:01:27 -0700

    Improve handling window resize events

--- app/lib/nhl/components/nhl-shot-chart/nhl-shot-chart.tsx
+++ app/lib/nhl/components/nhl-shot-chart/nhl-shot-chart.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import * as d3 from 'd3';
 
 export interface NHLShotChartProps {
@@ -15,33 +15,13 @@ export default function NHLShotChart({ shots }: NHLShotChartProps) {
   if (!shots || shots.length === 0) return null;
 
   const svgRef = useRef<SVGSVGElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [containerSize, setContainerSize] = useState({ width: 0, height: 0 });
 
-  // All distances are in FT from https://github.com/war-on-ice/icerink/blob/master/assets/js/rinkPlot.js
-  const RINK_CONFIG =
-  {
-    RINK_LENGTH: 200,
-    RINK_WIDTH: 85,
-    BLUE_LINE_WIDTH: 1,
-    BOARDS_RADIUS: 28,
-    RED_TO_BOARDS: 11,
-    RED_TO_FACEOFF: 20,
-    FACEOFF_RADIUS: 15,
-    FACEOFF_DOT_RADIUS: 1,
-    ZONE_LINE_WIDTH: (2 / 12),
-    CREASE_RADIUS: 6,
-    ZONE_LENGTH: 75,
-    ZONE_TO_NEUTRAL_DOT: 5,
-    CENTER_TO_NEUTRAL_DOT: 22,
-    REF_CREASE_RADIUS: 10,
-    CREASE_HEIGHT: 4,
-    FACEOFF_HOR_LENGTH: 3,
-    FACEOFF_VER_LENGTH: 4,
-    FACEOFF_HOR_DIST_CEN: 2,
-    FACEOFF_VER_DIST_CEN: (9 / 12),
-    FACEOFF_OUT_MARK_LENGTH: 2,
-    FACEOFF_OUT_MARK_DIST_BW: 5 + (7 / 12),
-    TRAPEZOID_TOP: 22,
-    TRAPEZOID_BOTTOM: 28
+  const handleResize = () => {
+    const { clientWidth, clientHeight } = containerRef.current;
+    console.log(`Setting container size to ${clientWidth} x ${clientHeight}`)
+    setContainerSize({ width: clientWidth, height: clientHeight });
   };
 
   useEffect(() => {
@@ -51,21 +31,20 @@ export default function NHLShotChart({ shots }: NHLShotChartProps) {
     svg.selectAll('*').remove();
 
     // Calculate the center point and padding
-    const svgWidth = svgRef.current?.clientWidth || 0;
-    const svgHeight = svgRef.current?.clientHeight || 0;
+    const svgWidth = containerSize.width;
+    const svgHeight = containerSize.height;
     const padding = 30;
 
     // Calculate scales for x and y values with padding
-    // See https://us.v-cdn.net/6030995/uploads/lithium_attachments/2345i9C9ED39EA4669452.jpg for a great visualization overlaid on a hockey rink
-    const xMin = -100
-    const xMax = 100
-    const yMin = -42.5
-    const yMax = 42.5
+    const xMin = -100;
+    const xMax = 100;
+    const yMin = -42.5;
+    const yMax = 42.5;
 
     const scaleX = d3.scaleLinear().domain([xMin, xMax]).range([padding, svgWidth - padding]);
     const scaleY = d3.scaleLinear().domain([yMin, yMax]).range([svgHeight - padding, padding]);
 
-    // Create markings on the ice - See https://github.com/war-on-ice/icerink/blob/master/assets/js/rinkPlot.js for a D3 example from 8 years ago
+    // Create markings on the ice
 
     // Create groups for each shot
     const shotGroups = svg
@@ -91,36 +70,48 @@ export default function NHLShotChart({ shots }: NHLShotChartProps) {
     // Show text on mouseover
     shotGroups
       .on('mouseover', function () {
-        // @ts-ignore
         d3.select(this).select('circle').attr('r', 8); // Increase circle size on hover
-        // @ts-ignore
         d3.select(this).select('text').style('opacity', 1); // Show the text
       })
       .on('mouseout', function () {
-        // @ts-ignore
         d3.select(this).select('circle').attr('r', 5); // Restore circle size
-        // @ts-ignore
         d3.select(this).select('text').style('opacity', 0); // Hide the text
       });
-  }, [shots]);
+  }, [shots, containerSize]);
+
+  useEffect(() => {
+    // Initial size calculation
+    handleResize();
+
+    // Event listener for window resize
+    window.addEventListener('resize', handleResize);
+
+    // Clean up event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
     <>
       <h2>Shot chart with mock data</h2>
       <div
+        ref={containerRef}
         style={{
           width: '100%',
-          height: '0',
+          height: 0,
           paddingBottom: '44.98%', // Maintain the aspect ratio (387 / 861 * 100) to avoid distortion
           position: 'relative',
           backgroundImage: 'url(/horizontal-rink-861x387.png)',
           backgroundSize: 'cover',
-          backgroundRepeat: 'no-repeat'
+          backgroundRepeat: 'no-repeat',
         }}
       >
         <svg
           ref={svgRef}
           style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
+          width={containerSize.width}
+          height={containerSize.height}
         />
       </div>
     </>
