e63dbbeb1bb0f14b431864d65de86d179fd6bc00
Rob Brennan <rob@therobbrennan.com>
2023-06-24 17:52:50 -0700

    Generate mock data from an X range of -100 to 100 and a Y range of -42.5 to 42.5

--- app/lib/nhl/generateShots.ts
+++ app/lib/nhl/generateShots.ts
@@ -1,5 +1,5 @@
 import type { NHLShot } from "./components/nhl-shot-chart/nhl-shot-chart";
 
 export const generateShots = (): NHLShot[] => {
-  return [{ x: 0, y: 0 }, { x: 12, y: 34 }, { x: -22, y: -3 }];
+  return [{ x: -90, y: 23 }, { x: 12, y: 34 }, { x: -22, y: -3 }, { x: 86, y: -23 }, { x: 0, y: 0 }];
 };
